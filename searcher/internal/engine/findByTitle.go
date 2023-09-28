8386b76a676fa49cd8b8050270fbe6f8df2ca585
PELENIUM <i.d.chernyshov@yandex.ru>
2023-09-28 23:37:57 +0300

    made select sql requests

--- searcher/internal/engine/findByTitle.go
+++ searcher/internal/engine/findByTitle.go
@@ -1,4 +1,9 @@
 package engine
 
 // SQL req
-// SELECT FROM sites <row> WHERE lower(<row>) LIKE "%<substring>%";
+
+// for single row (url, title, html-code)
+// SELECT FROM sites * WHERE lower(<row>) LIKE "%<substring>%";
+
+// for row with array (keywords)
+// SELECT * FROM sites WHERE EXISTS (SELECT 1 FROM unnest(keywords) AS element WHERE lower(element::text) LIKE lower('%<substring>%'));
